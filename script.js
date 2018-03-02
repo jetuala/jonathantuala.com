917f7de2953ab06ea47e682d9a287b94bb771b86
Jonathan Tuala <jetuala@gmail.com>
2018-03-01 22:28:47 -0800

    homepage

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  alert("Document is ready.");
+}
+
+$(document).ready(function(){
+  var data = {
+  method: "getQuote",
+  lang: "en" ,
+  format:"jsonp",
+  dataType: "jsonp"
+  };
+
+  var callQuote = function() {
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", data, function(jd) {
+      $("#quote").html(jd.quoteText + "  - " + jd.quoteAuthor);
+    });
+  };
+
+  callQuote();
+
+});
+
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-dark-grey";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-dark-grey", "");
+    }
+}
